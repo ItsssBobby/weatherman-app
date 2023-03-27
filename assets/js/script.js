bfc8c888d8e7c1930227d59f25e79d392a41e40b
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-03-27 16:35:29 -0400

    added event listeners and function to search for a city.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,41 @@ const temperature = document.getElementById('temperature');
 const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
 const forecastCards = document.getElementById('forecast-cards');
+
+cityInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        searchCity(cityInput.value);
+    }
+});
+
+searchBtn.addEventListener('click', () => {
+    searchCity(cityInput.value);
+});
+
+searchHistoryList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+        searchCity(e.target.textContent);
+    }
+});
+
+function searchCity(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+        .then(response => response.json())
+        .then(data => {
+            cityName.textContent = `${data.name} (${new Date().toLocaleDateString()})`;
+            weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+            temperature.textContent = `Temperature: ${data.main.temp} °F`;
+            humidity.textContent = `Humidity: ${data.main.humidity}%`;
+            windSpeed.textContent = `Wind Speed: ${data.wind.speed} MPH`;
+
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`);
+        })
+        .then(response => response.json())
+        .then(data => {
+            displayForecast(data.list.filter((_, index) => index % 8 === 0));
+            addToSearchHistory(city);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
